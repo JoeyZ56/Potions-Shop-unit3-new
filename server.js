c832a31373075298b85de0408bb4ac039f29b654
username <Jzazzi56@gmail.comn>
2023-08-19 12:47:56 -0700

    divided server and app-server, added an ABOUT route

--- server.js
+++ server.js
@@ -1,46 +1,11 @@
 require('dotenv').config();
+
 require('./config/database');
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
 
-const app = express();
+const app = require('./app-server');
 
-// there's no need to mount express.urlencoded middleware
-// why is that?
-app.use(express.json());
-app.use((req, res, next) => {
-	res.locals.data = {};
-	next();
-});
-app.use(logger('dev'));
-// Configure both serve-favicon & static middleware
-// to serve from the production 'build' folder
-app.use(favicon(path.join(__dirname, 'public', 'favicon', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
+const PORT = process.env.PORT || 4008;
 
-// Check if token and create req.user
-app.use(require('./config/checkToken'));
-
-// Put API routes here, before the "catch all" route
-app.use('/api/users', require('./routes/api/users'));
-// Protect the API routes below from anonymous users
-const ensureLoggedIn = require('./config/ensureLoggedIn');
-console.log(ensureLoggedIn);
-app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
-app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
-
-// The following "catch all" route (note the *) is necessary
-// to return the index.html on all non-AJAX requests
-app.get('*', function (_, res) {
-	res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Configure to use port 3001 instead of 3000 during
-// development to avoid collision with React's dev server
-const port = process.env.PORT || 4008;
-
-app.listen(port, function () {
-	console.log(`Express app running on port ${port}`);
+app.listen(PORT, () => {
+	console.log(`I am listening on ${PORT}. We in the Building`);
 });
