c7b1e014ea84cbc3cde01c80483dcc1ec58bb065
username <Jzazzi56@gmail.comn>
2023-11-07 16:35:49 -0800

    changing port domain name

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('./config/database');
 
 const app = require('./app-server');
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 'https://potions-shop-unit3-new.vercel.app';
 
 app.listen(PORT, () => {
 	console.log(`I am listening on ${PORT}. We in the Building`);
